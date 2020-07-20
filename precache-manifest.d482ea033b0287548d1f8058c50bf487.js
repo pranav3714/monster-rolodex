784e2f073704c202a333180f9e61982ad1219cec
BlackSilver <blacksilver@localhost.localdomain>
2020-07-19 21:47:48 -0500

    Updates

---
+++ precache-manifest.d482ea033b0287548d1f8058c50bf487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2231f04bfdf98f65a23e29a7ef7aa5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b419bd234f0abedb0249",
+    "url": "/monster-rolodex/static/css/main.20937633.chunk.css"
+  },
+  {
+    "revision": "9d58979bd3bbfd49bd0b",
+    "url": "/monster-rolodex/static/js/2.31af3a65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.31af3a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b419bd234f0abedb0249",
+    "url": "/monster-rolodex/static/js/main.e434cb70.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
